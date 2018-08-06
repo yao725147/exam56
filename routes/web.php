5225f1cac338d9aaf58a9afedc98ea8de30d0a2b
yao725147 <yao725147@gmail.com>
2018-08-06 14:38:13 +0800

    安裝中文語系

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,14 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')
+        ->with('name', 'stu1')
+        ->with('say', '嗨！');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
